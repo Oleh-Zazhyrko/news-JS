609276587d532983425fb8335d6babad1178428c
Oleh Zazhyrko <kosovo1989@gmail.com>
2022-12-11 18:11:57 +0100

    feat: add interface for api

---
+++ src/types/index.ts
@@ -0,0 +1,9 @@
+export interface Source {
+    readonly id: string | null;
+    readonly name: string | null;
+    readonly description: string | null;
+    readonly url: string | null;
+    readonly category: string | null;
+    readonly language: string | null;
+    readonly country: string | null;
+}
