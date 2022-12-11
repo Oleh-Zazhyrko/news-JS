c9bc04833d61e0350189b5cae0324d235858884e
Oleh Zazhyrko <kosovo1989@gmail.com>
2022-12-11 17:48:41 +0100

    feat: add ts, eslint and webpack packages

--- src/components/controller/appLoader.js
+++ src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: 'be3d6074fe8b46648911734583bfc5d2', // получите свой ключ https://newsapi.org/
         });
     }
 }
